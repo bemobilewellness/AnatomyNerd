b9eea526d87cea0666e3733ae2c235e27960eb2d
Jessie Keck <jessie.keck@gmail.com>
2018-09-02 23:04:11 -0700

    Update intro intent with details about the anatomy fact functionality.

--- intents/intro.rb
+++ intents/intro.rb
@@ -3,9 +3,14 @@
 intent 'Intro' do
   response_text = [
     '<speak>',
-      'I can give you stretching tips by asking <break time="500ms" />',
-      'Anatomy Nerd give me a stretching tip <break time="500ms" />',
-      'You can also ask specifically for particular categories like neck stretching tips or desk stretching tips.',
+      'I currently have 2 enabled features. Stretching Tips and Anatomy Facts.',
+      'I can give you stretching tips if you say <break time="500ms" />',
+      'Alexa, ask Anatomy Nerd for a stretching tip <break time="500ms" />',
+      'You can also ask specifically for particular categories like hand stretching tips or desk stretching tips.',
+      '<break time="1250ms" />',
+      'I can also tell you the anatomy fact of the day if you say <break time="500ms" />',
+      'Alexa, ask Anatomy Nerd for the anatomy fact of the day <break time="500ms" />',
+      'You can also ask for a random anatomy fact.',
     '</speak>'
   ].join(' ')
 
