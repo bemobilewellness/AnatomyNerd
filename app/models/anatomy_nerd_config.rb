fe5a6bc2df9386a049281d147c41348c51ecb06a
Jessie Keck <jessie.keck@gmail.com>
2018-09-03 10:41:35 -0700

    Add capability of getting configurations remotely from GitHub.

--- app/models/anatomy_nerd_config.rb
+++ app/models/anatomy_nerd_config.rb
@@ -2,6 +2,8 @@
 
 class AnatomyNerdConfig
   ALLOWED_TYPES = %i[local github].freeze
+  REMOTE_CACHE_TTL = 5.minutes
+
   attr_reader :type
   def initialize(type: :local)
     raise ArgumentError unless ALLOWED_TYPES.include?(type.to_sym)
@@ -13,7 +15,8 @@ class AnatomyNerdConfig
     when :local
       local_source
     when :github
-      raise NotImplementedError
+      raise(ArgumentError, 'You must provide a "GITHUB_CONFIG_URL" env var') unless github_config_url
+      github_source
     end
   end
 
@@ -28,5 +31,15 @@ class AnatomyNerdConfig
 
   def local_source
     YAML.load_file('./config/anatomy_nerd.yml')
+  end
+
+  def github_source
+    Rails.cache.fetch('anatomy_nerd/remote_config', expires_in: REMOTE_CACHE_TTL) do
+      YAML.load(Faraday.get(github_config_url).body)
+    end
+  end
+
+  def github_config_url
+    ENV['GITHUB_CONFIG_URL']
   end
 end
